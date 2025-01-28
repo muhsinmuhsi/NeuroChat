69eb6ede30272c1bdf08efb8f1cc3500f6174439
muhzin ck <muhsinmuhsinmuhsi786@gmail.com>
2025-01-28 09:53:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="">
+      <p className='text-red-500'>tailwind ok </p> 
+      <h1 className='text-xl text-center'>jjj</h1> 
+    </div>
+      
+    </>
+  )
+}
+
+export default App
